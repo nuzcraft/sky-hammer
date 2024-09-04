5a0d4ef682d4443ff9bce253292997a34f8550a5
nuzcraft <nuzcraft@gmail.com>
2024-09-03 22:00:01 -0500

    working on hero animations

--- scenes/hero/hero.gd
+++ scenes/hero/hero.gd
@@ -1,13 +1,16 @@
 extends CharacterBody3D
 class_name Hero
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const LERP_VAL: float = 0.1
+@export var RUN_SPEED: float = 5.0
+@export var SPRINT_SPEED: float = 10.0
+var speed: float = RUN_SPEED
+const JUMP_VELOCITY: float = 4.5
+const ACCELLERATION: float = 0.3
+const FRICTION: float = 0.8
 
 @onready var camera_joint: SpringArm3D = $CameraJoint
 @onready var model: Node3D = $HeroModel
-#@onready var animation_tree: AnimationTree = hero_model.get_node("AnimationTree")
+@onready var animation_tree: AnimationTree = model.get_node("AnimationTree")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -27,18 +30,24 @@ func _physics_process(delta: float) -> void:
 	move_direction.z = Input.get_action_strength("down") - Input.get_action_strength("up")
 	move_direction = move_direction.rotated(Vector3.UP, camera_joint.rotation.y).normalized()
 	
+	if Input.is_action_just_pressed("sprint"): 
+		speed = SPRINT_SPEED
+	if Input.is_action_just_released("sprint"): 
+		speed = RUN_SPEED
+	
 	if move_direction:
-		velocity.x = move_direction.x * SPEED
-		velocity.z = move_direction.z * SPEED
+		velocity.x = move_toward(velocity.x, move_direction.x * speed, ACCELLERATION)
+		velocity.z = move_toward(velocity.z, move_direction.z * speed, ACCELLERATION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		velocity.z = move_toward(velocity.z, 0, FRICTION)
 
+	animation_tree.set("parameters/BS1D_Idle_Run/blend_position", Vector2(velocity.x, velocity.z).length() / RUN_SPEED)
 	move_and_slide()
 	
 	if velocity.length() > 0.2:
 		var look_direction = Vector2(velocity.z, velocity.x)
-		model.rotation.y = lerp_angle(model.rotation.y, look_direction.angle(), LERP_VAL)
+		model.rotation.y = lerp_angle(model.rotation.y, look_direction.angle(), ACCELLERATION)
 	
 func _process(delta: float) -> void:
 	camera_joint.position = position
