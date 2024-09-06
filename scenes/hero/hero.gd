a3ee60a122fe2e9a4cce8529e139ca3fe7f84e7d
nuzcraft <nuzcraft@gmail.com>
2024-09-05 22:21:38 -0500

    adding unsheath attack

--- scenes/hero/hero.gd
+++ scenes/hero/hero.gd
@@ -13,17 +13,35 @@ const FRICTION: float = 0.8
 @onready var animation_tree: AnimationTree = model.get_node("AnimationTree")
 @onready var animation_player: AnimationPlayer = model.get_node("AnimationPlayer")
 
-var sheathed: bool = true
+enum HERO_STATE{
+	SHEATHED,
+	SHEATHING,
+	UNSHEATHED,
+	UNSHEATHING,
+	UNSHEATH_ATTACKING
+}
+var STATE = HERO_STATE.SHEATHED
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("attack") and sheathed:
-		animation_tree.set("parameters/unsheath_shot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
-		animation_tree.set("parameters/sheath_state/transition_request", "unsheathed")
-		sheathed = false
-
+	match STATE:
+		HERO_STATE.SHEATHED:
+			moveable_state()
+		HERO_STATE.SHEATHING:
+			stopped_state()
+		HERO_STATE.UNSHEATHED:
+			moveable_state()
+		HERO_STATE.UNSHEATHING:
+			stopped_state()
+		HERO_STATE.UNSHEATH_ATTACKING:
+			stopped_state()
+	
+func _process(delta: float) -> void:
+	camera_joint.position = position
+	
+func moveable_state() -> void:
 	var move_direction := Vector3.ZERO
 	move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	move_direction.z = Input.get_action_strength("down") - Input.get_action_strength("up")
@@ -31,7 +49,7 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("sprint"): 
 		speed = SPRINT_SPEED
-	if Input.is_action_just_released("sprint"): 
+	if Input.is_action_just_released("sprint") and STATE == HERO_STATE.SHEATHED: 
 		speed = RUN_SPEED
 	
 	if move_direction:
@@ -41,12 +59,43 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, FRICTION)
 		velocity.z = move_toward(velocity.z, 0, FRICTION)
 
-	animation_tree.set("parameters/idle_run_blend/blend_amount", Vector2(velocity.x, velocity.z).length() / RUN_SPEED)
+	animation_tree.set("parameters/idle_run_blend/blend_amount", min(Vector2(velocity.x, velocity.z).length() / RUN_SPEED, 1.0))
+	animation_tree.set("parameters/idle_run_blend2/blend_amount", min(Vector2(velocity.x, velocity.z).length() / RUN_SPEED, 1.0))
 	move_and_slide()
 	
 	if velocity.length() > 0.2:
 		var look_direction = Vector2(velocity.z, velocity.x)
 		model.rotation.y = lerp_angle(model.rotation.y, look_direction.angle(), ACCELLERATION)
-	
-func _process(delta: float) -> void:
-	camera_joint.position = position
+		
+	if Input.is_action_just_pressed("attack"):
+		match STATE:
+			HERO_STATE.SHEATHED:
+				if velocity.length() < 0.2:
+					animation_tree.set("parameters/unsheath_shot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+					STATE = HERO_STATE.UNSHEATHING
+				else:
+					animation_tree.set("parameters/unsheath_attack_shot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+					STATE = HERO_STATE.UNSHEATH_ATTACKING
+				animation_tree.set("parameters/sheath_state/transition_request", "unsheathed")
+				animation_tree.set("parameters/idle_run_blend/blend_amount", 0)
+				animation_tree.set("parameters/idle_run_blend2/blend_amount", 0)
+	elif Input.is_action_just_pressed("sheath"):
+		match STATE:
+			HERO_STATE.UNSHEATHED:
+				animation_tree.set("parameters/sheath_shot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+				animation_tree.set("parameters/sheath_state/transition_request", "sheathed")
+				animation_tree.set("parameters/idle_run_blend/blend_amount", 0)
+				animation_tree.set("parameters/idle_run_blend2/blend_amount", 0)
+				STATE = HERO_STATE.SHEATHING
+			
+func stopped_state() -> void:
+	match STATE:
+		HERO_STATE.SHEATHING:
+			if not animation_tree.get("parameters/sheath_shot/active"):
+				STATE = HERO_STATE.SHEATHED
+		HERO_STATE.UNSHEATHING:
+			if not animation_tree.get("parameters/unsheath_shot/active"):
+				STATE = HERO_STATE.UNSHEATHED
+		HERO_STATE.UNSHEATH_ATTACKING:
+			if not animation_tree.get("parameters/unsheath_attack_shot/active"):
+				STATE = HERO_STATE.UNSHEATHED
