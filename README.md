f5cd5c65d3d4c01aabb068431e489e51cd33ffe6
nuzcraft <nuzcraft@gmail.com>
2024-09-16 21:51:50 -0500

    worked on some level design

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 game by nuzcraft
 hero model + animations by nuzcraft
 all other models by quaternius
+skybox by emil persson
+grass by icterus https://github.com/IcterusGames/SimpleGrassTextured
--- main.gd
+++ main.gd
@@ -7,7 +7,8 @@ const BLOOD = preload("res://scenes/blood.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	hero.attack_landed.connect(_on_hero_attack_landed)
-
+	SimpleGrass.set_interactive(true)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	for enemy in get_tree().get_nodes_in_group("Enemies"):
