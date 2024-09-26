e3ac2f2fc6e592890fce3e31c7cdcffd291e4c0d
nuzcraft <nuzcraft@gmail.com>
2024-09-26 13:39:37 -0500

    spruced up a few things

--- scenes/control/end.gd
+++ scenes/control/end.gd
@@ -1,10 +1,21 @@
 extends Control
-
+const MAIN = preload("res://main.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if OS.get_name() == "Web":
+		$VBoxContainer/HBoxContainer/QuitButton.hide()
+		$VBoxContainer/HBoxContainer/Control.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_packed(MAIN)
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
--- scenes/levels/level_1.gd
+++ scenes/levels/level_1.gd
@@ -5,7 +5,7 @@ signal portal_entered
 
 @onready var portal: Node3D = $Portal
 
-# Called when the node enters the scene tree for the first time.
+ #Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	portal.body_entered.connect(_on_body_entered_portal)
 
